b25822e66315d8b37a54079da9532fc9967bd904
TheWhoAreYouPerson <thewhoareyouperson@gmail.com>
2018-07-08 20:23:00 -0500

    more relaxed eslint rules, and make the codebase style non-erroring

--- scripts/shaders.js
+++ scripts/shaders.js
@@ -27,64 +27,69 @@ void main()
 }
 `;
 
-// Creates a shader program with default shaders
-function initDefaultShaderProgram(gl){
-  return initShaderProgram(gl, vsSource, fsSource);
+
+// Requests and returns a fully compiled shader object
+function loadShader(gl, type, source)
+{
+	const shader = gl.createShader(type);
+
+	// Send the source to the shader object we just initialized
+	gl.shaderSource(shader, source);
+
+	// Compile the shader program
+	gl.compileShader(shader);
+
+	// See if it compiled successfully
+	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
+	{
+		alert(`An error occurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`);
+		gl.deleteShader(shader);
+		return undefined;
+	}
+
+	return shader;
 }
 
 // Creates a shader program
-function initShaderProgram(gl, vsSource, fsSource){
-  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
-  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+function initShaderProgram(gl, vertString, fragString)
+{
+	const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vertString);
+	const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fragString);
 
-  // Create the shader program
-  const shaderProgram = gl.createProgram();
-  gl.attachShader(shaderProgram, vertexShader);
-  gl.attachShader(shaderProgram, fragmentShader);
-  gl.linkProgram(shaderProgram);
+	// Create the shader program
+	const shaderProgram = gl.createProgram();
+	gl.attachShader(shaderProgram, vertexShader);
+	gl.attachShader(shaderProgram, fragmentShader);
+	gl.linkProgram(shaderProgram);
 
-  // If creating the shader program failed, alert
-  if(!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)){
-    alert("Unable to initialize the shader program: " + gl.getProgramInfoLog(shaderProgram));
-    return;
-  }
+	// If creating the shader program failed, alert
+	if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS))
+	{
+		return alert(`Unable to initialize the shader program: ${gl.getProgramInfoLog(shaderProgram)}`);
+	}
 
-  return shaderProgram;
+	return shaderProgram;
 }
 
-// Requests and returns a fully compiled shader object
-function loadShader(gl, type, source){
-  const shader = gl.createShader(type);
-
-  // Send the source to the shader object we just initialized
-  gl.shaderSource(shader, source);
-
-  // Compile the shader program
-  gl.compileShader(shader);
-
-  // See if it compiled successfully
-  if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
-  {
-    alert("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));
-    gl.deleteShader(shader);
-    return;
-  }
-
-  return shader;
+// Creates a shader program with default shaders
+function initDefaultShaderProgram(gl)
+{
+	return initShaderProgram(gl, vsSource, fsSource);
 }
+
 
 function getProgramInfo(gl, shaderProgram)
 {
-  return {
-    program: shaderProgram,
-    attribLocations: {
-      vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
-      textureCoord: gl.getAttribLocation(shaderProgram, "aTextureCoord"),
-    },
-    uniformLocations:{
-      projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
-      modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
-      uSampler: gl.getUniformLocation(shaderProgram, "uSampler"),
-    },
-  };
+	return {
+		program: shaderProgram,
+		attribLocations: {
+			vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+			textureCoord: gl.getAttribLocation(shaderProgram, 'aTextureCoord'),
+		},
+		uniformLocations: {
+			projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+			modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+			uSampler: gl.getUniformLocation(shaderProgram, 'uSampler'),
+		},
+	};
 }
